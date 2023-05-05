b16027db75ef1167be147290a97f2bed28195894
retrok218 <retrok218@gmail.com>
2023-05-05 17:58:09 -0600

    ok

--- public/js/datatable.js
+++ public/js/datatable.js
@@ -181,6 +181,34 @@ var table = $('#juiciotabla').DataTable({
 
 
     });
+
+
+    (function (document, window, index) {
+        console.log(document);
+        var inputs = document.querySelectorAll(".inputfile");
+        Array.prototype.forEach.call(inputs, function (input) {
+          var label = input.nextElementSibling,
+            labelVal = label.innerHTML;
+      
+          input.addEventListener("change", function (e) {
+            var fileName = "";
+            if (this.files && this.files.length > 1)
+              fileName = (this.getAttribute("data-multiple-caption") || "").replace(
+                "{count}",
+                this.files.length
+              );
+            else fileName = e.target.value.split("\\").pop();
+      
+            if (fileName) label.querySelector("span").innerHTML = fileName;
+            else label.innerHTML = labelVal;
+          });
+        });
+      })(document, window, 0);
+
+
+
+
+
 });
 
 function mostrar_modal_juicio(data) {
@@ -253,4 +281,6 @@ function update_actualiza_datos_generales(id,formname){
 
 //    location.reload(); // recarga la pagina al cuncluir la edicion dentro del modal               
 }
+
+
 
