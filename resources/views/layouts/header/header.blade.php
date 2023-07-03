0b4acae0efd6c132f85aa2c9b3a8f24053521d24
retrok218 <retrok218@gmail.com>
2023-07-03 10:16:33 -0600

    graficas en pros

--- resources/views/layouts/header/header.blade.php
+++ resources/views/layouts/header/header.blade.php
@@ -138,19 +138,19 @@
                             <form action="{{ url('login') }}" method="POST">
                                 @csrf
                                 <div class="form-group">
-                                    <label for="exampleDropdownFormEmail2">Email address</label>
+                                    <label for="emailInput">Email address</label>
                                     <input type="email" class="form-control" id="emailInput" placeholder="email@example.com"  name="email" value="{{old('email')}}">
                                     @error('email')<div style="color:red">{{$message}}</div>@enderror
                                   </div>
                                   <div class="form-group">
-                                    <label for="exampleDropdownFormPassword2">Password</label>
+                                    <label for="passwordInput">Password</label>
                                     <input type="password" class="form-control" id="passwordInput" placeholder="Password" name="password">
                                     @error('password') <div style="color:red">{{$message}}</div>@enderror
                                   </div>
                                   <div class="form-check">
-                                    <input type="checkbox" class="form-check-input"  name="remember">
-                                    <label class="form-check-label" for="dropdownCheck2">
-                                      Remember me
+                                    <input type="checkbox" class="form-check-input"  name="remember" id="rememberm">
+                                    <label class="form-check-label" for="rememberm">
+                                      Recuerdame
                                     </label>
                                   </div>
                                   <div class="form-group">
